321f054584bd1367a1e59d8504eef797681155c0
Teresa Plaz <teresaplaz@gmail.com>
2018-09-14 21:22:38 -0400

    trying again and again

---
+++ server/app.js
@@ -0,0 +1,99 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const cors         = require('cors');
+const session      = require('express-session');
+const passport     = require('passport');
+const MongoStore   = require('connect-mongo')(session);
+
+
+
+
+mongoose.Promise = Promise;
+mongoose
+  .connect('mongodb://localhost/Frank', {useMongoClient: true})
+  .then(() => {
+    console.log( 'Connected to Mongo!' );
+  }).catch(err => {
+    console.error( 'Error connecting to mongo', err );
+  });
+
+const app_name = require('./package.json').name;
+const debug = require( 'debug' )( `${app_name}:${path.basename( __filename ).split( '.' )[ 0 ]}` );
+const passportSetup = require('./configs/passport');
+passportSetup(passport);
+
+const app = express();
+
+// Middleware Setup
+app.use(session({
+  secret: 'angular auth passport',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection }),
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: ["http://localhost:4200"]
+ }));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(
+  (req, res, next) => {
+    res.header('Access-Control-Allow-Credential', true);
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+  }
+ );
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+
+//ROUTES
+const functions = require('./routes/friki-functions');
+const auth = require( './routes/auth-routes' );
+const userCrud = require( './routes/user' );
+
+
+app.use('/functions', functions);
+app.use( '/auth', auth );
+app.use( '/user', userCrud );
+
+
+// This will be the default route is nothing else is caught
+app.use(function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+module.exports = app;
---
+++ server/routes/friki-functions.js
@@ -0,0 +1,159 @@
+const express = require('express');
+const router = express.Router();
+const passport   = require('passport');
+const firmata = require("firmata");
+const mongoose = require('mongoose');
+// const Messages = require('./../models/Message');
+let five = require("johnny-five"), board, lcd, led, servin, running;
+// EtherPortClient INIT 
+const EtherPortClient = require('etherport-client').EtherPortClient;
+
+//WIFI PORT
+//   let port = new EtherPortClient({
+//     host: '192.168.43.46', //'192.168.0.16',  
+//     port: 3030
+//   });
+
+//ARDUINO WIFI
+// //ESP8622
+// board = new firmata.Board(port);
+// board.once("ready", function() 
+// {
+//   console.log("ready");
+//   board.isReady = true;
+
+//   //Virtual board to access with Johnny5
+//   let Franky = new five.Board({io: board});
+//   Franky.on("ready", function()
+//   {
+//     console.log("five ready");
+//     led = new five.Led.RGB({pins: {green:5,red: 16,blue: 4}}); // | G-D1 => 5 | R-D0 => 16 | B-D2 => 4 |  
+//   });
+// });
+
+//ARDUINO CABLE
+let franki = new five.Board();
+franki.on("ready", function(){
+    lcd = new five.LCD({
+      // LCD pin name RS EN DB4 DB5 DB6 DB7
+      // Arduino pin # 7  8  9  10 11 12
+      pins: [7, 8, 9, 10, 11, 12],
+      backlight: 6,
+      rows: 2,
+      cols: 16
+    });
+
+    servin = new five.Servo({
+      pin: 3,
+      // center: true,
+      range: [45, 135]
+    }); 
+});
+
+
+// FUNCTION 01 BLINK
+router.get('/1', (req,res,next) => {
+  // console.log("holi");
+  led.blink(300);
+  res.json("Funtion1 - ON");
+});
+
+// FUNCTION 02 OFF
+router.get('/2', (req,res,next) => {
+  led.stop().off();
+  res.json("Funtion2 - OFF");
+});
+
+// FUNCTION 03 RED
+router.get('/3', (req,res,next) => {
+  led.stop();
+  led.color('red'); // <===== [COLOR]
+  
+  res.json("Funtion3 - RED");
+});
+
+// FUNCTION 04 BLUE
+router.get('/4', (req,res,next) => {
+  led.stop();
+  led.color('blue'); // <===== [COLOR]
+
+  res.json("Funtion4 - BLUE");
+    
+});
+
+// FUNCTION 05 GREEN
+router.get('/5', (req,res,next) => {
+  led.stop();
+  led.color('green'); // <===== [COLOR]
+  res.json('Function5 -- GREEN');
+});
+
+// FUNCTION 06 TEXT
+router.post('/6', (req,res,next) => {
+    lcd.clear();
+  const { text } = req.body;
+  console.log(text);
+    // led.stop().off();
+    lcd.cursor(0, 0).print("Your text: ");
+    lcd.cursor(1, 0).print(text);
+  
+  // setTimeout(function() {
+  //   lcd.clear();
+  //   lcd.cursor(0, 0).print(`lo escribo aqui`);
+  // }, 2000);
+  // Message.findById(req.params.id)
+    //     .then(message => {
+    //         res.json(message);
+    //     })
+    //     .catch(error => next(error));
+
+  res.json("Function6 - Text");
+});
+
+// FUNCTION 07 RUNNING-MAN
+router.get('/7', (req,res,next) => {
+  lcd.clear();
+  servin.stop();
+  // led.stop().off();
+  let frame = 1;
+  let frames = [":runninga:", ":runningb:"];
+  let row = 1;
+  let col = 0;
+  lcd.useChar("runninga");
+  lcd.useChar("runningb");
+
+ running =  setInterval(function() 
+    {
+      lcd.home().print("The Running Man"); 
+      lcd.clear().cursor(row, col).print(
+        frames[frame ^= 1]);
+        lcd.home().print("The Running Man");    
+  
+      if (++col === lcd.cols) {
+        col = 0;
+        // if (++row === lcd.rows) {
+        //   row = 0;
+        // }
+      }
+    }, 300); 
+  
+  res.json("Function7 - Running_Man");
+});
+
+// FUNCTION 08 SERVO ON
+router.get('/8', (req,res,next) => {
+  clearInterval(running);
+  // lcd.clear();
+  servin.sweep();
+  // led.stop().off();
+  res.json("Funtion8 - SERVO ON");
+});
+
+// FUNCTION 09 SERVO OFF
+router.get('/9', (req,res,next) => {
+  servin.stop();
+  // led.stop().off();
+  res.json("Funtion9 - SERVO OFF");
+});
+
+module.exports = router;
